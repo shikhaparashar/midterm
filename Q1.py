78d6a3e42e83827d30c0184a7930837687557830
shikhaparashar <47656081+shikhaparashar@users.noreply.github.com>
2019-04-02 15:07:39 +0530

    Add files via upload
    method of strings

---
+++ Q1.py
@@ -0,0 +1,7 @@
+ANSWER1:
+METHOD OF STRINGS:
+(1)Capitalise(a.capitalise()):It is used to convert the string into capital form.
+(2)Title(a.title()):this function is used to convert the sting into title i.e only first letter of the title becomes capital.
+(3)count(a.count()):this function is used to count the no. of the total characters in a strings.
+(4)isdigit(a.isdigit()):this functions returns true value only if the string is consist of digits.
+
